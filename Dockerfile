15ffb9e62ae8db176ece0d5bd530eb86c7ce33f6
Aad Versteden <madnificent@gmail.com>
2025-07-03 19:50:47 +0200

    Generate some QR code

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM semtech/mu-javascript-template:1.8.0
+FROM semtech/mu-javascript-template:feature-simplify-build-process
 LABEL maintainer="Aad Versteden <madnificent@gmail.com>"
+
+RUN apt-get update; apt-get -y upgrade; apt-get -y install qrencode
 
 # see https://github.com/mu-semtech/mu-javascript-template for more info
--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // see https://github.com/mu-semtech/mu-javascript-template for more info
-
 import { app, query, errorHandler } from 'mu';
+import { exec } from 'child_process';
 
 app.get('/', function( req, res ) {
-  res.send('Hello mu-javascript-template');
-} );
+  exec(`qrencode "${req.query.string.replace(/"/g,'\\"')}" -o /share/qrcode.png; sync`, (_err, stdout, _stderr) => {
+    res.send(stdout);
+  });
+});
 
 app.use(errorHandler);
